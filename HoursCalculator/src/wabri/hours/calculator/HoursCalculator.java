6544ccbb9cacd60d2ce423977c3be12bf04ba0e7
Wabri <gabriele.puliti@gmail.com>
2018-07-09 12:40:36 +0200

    update

--- HoursCalculator/src/wabri/hours/calculator/HoursCalculator.java
+++ HoursCalculator/src/wabri/hours/calculator/HoursCalculator.java
@@ -8,15 +8,25 @@ import java.io.IOException;
 public class HoursCalculator {
 
 	private String fileName;
+	private int firstMonth;
+	private int lastMonth;
 
-	public HoursCalculator(String fileName) {
+	public HoursCalculator(String fileName, int firstMonth, int lastMonth) {
 		this.fileName = fileName;
+		this.firstMonth = firstMonth;
+		this.lastMonth = lastMonth;
 	}
 
 	public void run() {
 		Double totalHours = new Double(0);
 		int totalDays = 0;
+		Double totalHoursPerMonth[][] = new Double[12][2];
 		String lastDay = new String("");
+
+		for (int i = 0; i < totalHoursPerMonth.length; i++) {
+			totalHoursPerMonth[i][0] = 0.0;
+			totalHoursPerMonth[i][1] = 0.0;
+		}
 
 		try (FileReader reader = new FileReader(fileName)) {
 			BufferedReader bufferedReader = new BufferedReader(reader);
@@ -28,10 +38,18 @@ public class HoursCalculator {
 
 			while ((line = bufferedReader.readLine()) != null) {
 				if (line.startsWith("##")) {
-					totalHours += Integer.parseInt(String.valueOf(line.charAt(23)));
+					double actualDayHours = Integer.parseInt(String.valueOf(line.charAt(23)));
 					if (line.charAt(24) == ',') {
-						totalHours += 0.1 * Integer.parseInt(String.valueOf(line.charAt(25)));
+						actualDayHours += 0.1 * Integer.parseInt(String.valueOf(line.charAt(25)));
 					}
+					if (Integer.parseInt(String.valueOf(line.charAt(14))) == 1) {
+						totalHoursPerMonth[Integer.parseInt(1 + String.valueOf(line.charAt(15)))][0] += actualDayHours;
+						totalHoursPerMonth[Integer.parseInt(1 + String.valueOf(line.charAt(15)))][1]++;
+					} else {
+						totalHoursPerMonth[Integer.parseInt(0 + String.valueOf(line.charAt(15)))][0] += actualDayHours;
+						totalHoursPerMonth[Integer.parseInt(0 + String.valueOf(line.charAt(15)))][1]++;
+					}
+					totalHours += actualDayHours;
 					totalDays++;
 					lastDay = line.substring(11, 19);
 				}
@@ -46,10 +64,13 @@ public class HoursCalculator {
 		try {
 			FileWriter writer = new FileWriter("totalDaysHours.md", false);
 			writer.write("## Totale ore e giorni di tirocinio");
-			writer.write("\r\n");
-			writer.write("#### Totale ore: " + totalHours);
-			writer.write("\r\n");
-			writer.write("#### Totale giorni: " + totalDays);
+			writer.write("\n\r");
+			writer.write("| Mese | Ore | Giorni  |\r\n" + "| ------------- |:-------------:| -----:|\r\n");
+			for (int i = firstMonth; i <= lastMonth; i++) {
+				writer.write("| " + getMonthFromInt(i) + " | " + totalHoursPerMonth[i][0] + "| "
+						+ (int) Math.round(totalHoursPerMonth[i][1]) + "| \r\n");
+			}
+			writer.write("| Totale | " + totalHours + "| " + totalDays + "| \r\n");
 			writer.write("\r\n");
 			writer.write("#### Ultimo aggiornamento: " + lastDay);
 			writer.write("\r\n");
@@ -64,8 +85,41 @@ public class HoursCalculator {
 
 	}
 
+	private String getMonthFromInt(int i) {
+		switch (i) {
+		case 1:
+			return "Gennaio";
+		case 2:
+			return "Febbraio";
+		case 3:
+			return "Marzo";
+		case 4:
+			return "Aprile";
+		case 5:
+			return "Maggio";
+		case 6:
+			return "Giugno";
+		case 7:
+			return "Luglio";
+		case 8:
+			return "Agosto";
+		case 9:
+			return "Settembre";
+		case 10:
+			return "Ottobre";
+		case 11:
+			return "Novembre";
+		case 12:
+			return "Dicembre";
+		default:
+			break;
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
-		HoursCalculator hoursCalculator = new HoursCalculator(args[0]);
+		HoursCalculator hoursCalculator = new HoursCalculator(args[0], Integer.parseInt(args[1]),
+				Integer.parseInt(args[2]));
 		hoursCalculator.run();
 	}
 
--- HoursCalculator/totalDaysHours.md
+++ HoursCalculator/totalDaysHours.md
@@ -1,6 +1,13 @@
-## Totale ore e giorni calcolati con HoursCalculator
-#### Totale ore: 323.9
-#### Totale giorni: 56
-#### Ultimo aggiornamento: 02/05/18
+## Totale ore e giorni di tirocinio
+| Mese | Ore | Giorni  |
+| ------------- |:-------------:| -----:|
+| Maggio | 80.0| 13| 
+| Giugno | 53.5| 9| 
+| Luglio | 9.5| 2| 
+| Agosto | 0.0| 0| 
+| Settembre | 0.0| 0| 
+| Totale | 143.0| 24| 
+
+#### Ultimo aggiornamento: 05/07/18
 <!-- Per aggiornare eseguire il jar HoursCalculator.jar -->
-***Questo file è autogenerato dallo script ./runHoursCalculator.sh***
+*Questo file è autogenerato da HoursCalculator*
